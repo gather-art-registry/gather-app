6228f96f82a60522297586e7f20404d7d57b2b83
Rosie Hoyem <rosiehoyem@gmail.com>
2015-03-03 19:25:36 -0600

    moved font folder to app/assets and fixed syntax in roboto.scss

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,9 @@ module GatherArtRegistry
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     # config.autoload_paths += %W(#{config.root}/spec/support)
 
+    config.serve_static_assets = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
